ff2db0513451195add79b3f9921a75405092115e
jack <jacknh418@gmail.com>
2020-03-21 16:00:20 +0800

    added add new column functionality

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -13,9 +13,22 @@
       right
       color="cyan accent-2"
       style="bottom: 20px"
+      @click="openDialog"
     >
       <v-icon>mdi-plus</v-icon>
     </v-btn>
+    <v-dialog v-model="dialog" max-width="500px" @click:outside="closeDialog">
+      <v-card style="padding-top: 40px">
+        <v-card-text>
+          <v-text-field
+            label="New Column Name"
+            outlined
+            v-model="newColumnName"
+            @keydown.enter="closeDialog"
+          ></v-text-field>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
     <router-view v-if="isTaskOpen" />
   </v-container>
 </template>
@@ -33,13 +46,20 @@ export default {
   },
   data() {
     return {
-      newColumnName: ""
+      newColumnName: "",
+      dialog: false
     };
   },
   methods: {
-    createColumn() {
+    closeDialog() {
+      if (this.newColumnName === "") return;
       this.$store.commit("CREATE_COLUMN", { name: this.newColumnName });
       this.newColumnName = "";
+      this.dialog = false;
+    },
+    openDialog() {
+      this.newColumnName = "";
+      this.dialog = true;
     }
   }
 };
