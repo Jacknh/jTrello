4a45c0abae1f88d352388fee7f2d4f3e2b7eb175
jack <jacknh418@gmail.com>
2020-04-17 19:32:52 +0800

    app navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <v-app>
+    <AppNav />
     <router-view />
   </v-app>
 </template>
+
+<script>
+import AppNav from "./components/AppNav";
+export default {
+  components: { AppNav }
+};
+</script>
 
 <style lang="scss"></style>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="wrapper">
+    <div class="title"><i class="fab fa-trello"></i> jTrello</div>
+    <div>
+      <i class="fas fa-user-circle avatar" @click.stop="dropdown = true"></i>
+      <ul v-if="dropdown" class="dropdown" @click.stop>
+        <li class="first-li">Jack Zhang</li>
+        <li class="last-li" @click="dropdown = false">
+          <i class="fas fa-sign-out-alt"></i> Signout
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      dropdown: false,
+    };
+  },
+  watch: {
+    dropdown(val) {
+      if (val) {
+        window.addEventListener("click", () => {
+          this.dropdown = false;
+        });
+      } else {
+        window.removeEventListener("click");
+      }
+    },
+  },
+  methods: {},
+};
+</script>
+<style lang="scss" scoped>
+$bg-color: #38a89d;
+
+.wrapper {
+  display: flex;
+  justify-content: space-between;
+  padding: 15px 25px;
+  background: $bg-color;
+  box-shadow: 0px 10px 20px #38a89d;
+  color: #fff;
+
+  .title {
+    font-size: 1.6rem !important;
+    cursor: pointer;
+  }
+
+  .avatar {
+    font-size: 30px;
+    cursor: pointer;
+  }
+
+  .dropdown {
+    z-index: 1;
+    position: absolute;
+    right: 15px;
+    background: #fff;
+    color: #222;
+    list-style-type: none;
+    padding: 10px 15px;
+    border-radius: 4px;
+
+    .first-li {
+      margin-bottom: 5px;
+    }
+    .last-li {
+      cursor: pointer;
+    }
+  }
+}
+</style>
