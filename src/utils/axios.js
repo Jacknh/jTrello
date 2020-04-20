1b6c9be457a5d725d419a443493bee683d859fb7
jack <jacknh418@gmail.com>
2020-04-20 18:13:18 +0800

    401 issue reload

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -11,6 +11,7 @@ instance.interceptors.response.use(
   (error) => {
     if (error.response.status === 401) {
       store.dispatch('logout');
+      location.reload();
     }
 
     return Promise.reject(error)
