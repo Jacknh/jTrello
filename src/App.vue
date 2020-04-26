a0f1a3bf1e5943ef3cc0d60577a301d9c5f28086
jack <jacknh418@gmail.com>
2020-04-26 09:23:00 +0800

    mobile detect

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <v-app>
     <AppNav />
+    <p class="mobile__warning" v-if="mobile">
+      It seems like you are using a mobile device, switching to a computer would
+      gives you a better user experience.
+    </p>
     <notifications
       group="auth"
       position="top center"
@@ -9,7 +13,9 @@
     >
       <template slot="body" slot-scope="props">
         <div class="notification__text__wrapper">
-          <div><i class="fas fa-exclamation-circle"></i> {{props.item.text}}</div>
+          <div>
+            <i class="fas fa-exclamation-circle"></i> {{ props.item.text }}
+          </div>
         </div>
       </template>
     </notifications>
@@ -18,8 +24,17 @@
 </template>
 
 <script>
+import { mobileAndTabletCheck } from "./utils/helpers";
 import AppNav from "./components/AppNav";
 export default {
+  created() {
+    this.mobile = mobileAndTabletCheck();
+  },
+  data() {
+    return {
+      mobile: false,
+    };
+  },
   components: { AppNav },
 };
 </script>
@@ -30,5 +45,12 @@ export default {
   background: red;
   border-radius: 4px;
   padding: 6px 10px;
+}
+.mobile__warning {
+  color: yellow;
+  background: rebeccapurple;
+  margin: 0;
+  padding: 5px;
+  text-align: center;
 }
 </style>
